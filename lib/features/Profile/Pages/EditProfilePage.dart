acecd9247fcfc6d672901de987f73eb7179cfd5f
ZiadElsewedy <ziadelsewedy1@gmai.com>
2025-05-02 20:00:14 +0300

    last update

--- lib/features/Profile/Pages/EditProfilePage.dart
+++ lib/features/Profile/Pages/EditProfilePage.dart
@@ -16,6 +16,19 @@ class EditProfilePage extends StatefulWidget {
 
 class _EditProfilePageState extends State<EditProfilePage> {
   final BioTextcontroller = TextEditingController();
+  void updateProfilePage ()  {
+    if (BioTextcontroller.text.isNotEmpty) {
+      final profileCubit  = context.read<ProfileCubit>();
+      profileCubit.updateUserProfile(
+        widget.user.id, 
+        BioTextcontroller.text,
+        widget.user.profilePictureUrl.isNotEmpty ? widget.user.profilePictureUrl : "",
+      ); 
+    } else {
+      // Handle empty bio case if needed
+    }
+    
+  }
   //build UI
   @override
   Widget build(BuildContext context) {
@@ -37,16 +50,31 @@ class _EditProfilePageState extends State<EditProfilePage> {
     // Initialize the text controller with the current bio
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Edit Profile"),
+        title: const Text("Edit Profile" , style: TextStyle(color: Color.fromARGB(255, 95, 95, 95)),),
         centerTitle: true,
+        actions: [ 
+          IconButton(
+            icon: const Icon(Icons.upload, color: Color.fromARGB(255, 95, 95, 95),),
+            onPressed: () {
+              // Handle save button press
+              // You can call a method to save the changes here
+              // For example:
+              // ProfileCubit.get(context).updateBio(BioTextcontroller.text);
+            },
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            MyTextField(controller: BioTextcontroller,
-             hintText: widget.user.bio.isEmpty ? "Empty bio .." : widget.user.bio,
-              obsecureText:false ),
+            Text("Bio", style: TextStyle(fontSize: 20, color: Color.fromARGB(255, 0, 0, 0)),),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: MyTextField(controller: BioTextcontroller,
+               hintText: widget.user.bio.isEmpty ? "Empty bio .." : widget.user.bio,
+                obsecureText:false ),
+            ),
             
              SizedBox(height: 20),
             
