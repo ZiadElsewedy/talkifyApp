948e384fdfc1366b968da6228aa0690142ebf49c
ZiadElsewedy <ziadelsewedy1@gmai.com>
2025-05-04 15:17:30 +0300

    Filebase_Storage_repo Created

--- lib/Storage/Domain/Storage_repo.dart
+++ lib/Storage/Domain/Storage_repo.dart
@@ -1,7 +1,9 @@
+import 'dart:typed_data';
+
 abstract class StorageRepo {
   // upload profile image on mobile 
-  Future<String> uploadProfileImageMobile(String imagePath , String fileName);
-  Future<String> uploadProfileImageWeb(String imagePath , String fileName);
+  Future<String?> uploadProfileImageMobile(String imagePath , String fileName);
+  Future<String?> uploadProfileImageWeb(Uint8List fileBytes  , String fileName);
   //
   // uplaod image on web 
 }
