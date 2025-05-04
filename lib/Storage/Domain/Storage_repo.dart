65952793edb6d5e09feb840f3a6f97c72d9cd64f
ZiadElsewedy <ziadelsewedy1@gmai.com>
2025-05-04 15:14:16 +0300

    Inside Storage Folder we created storage repo file into domain folder

--- lib/Storage/Domain/Storage_repo.dart
+++ lib/Storage/Domain/Storage_repo.dart
@@ -2,5 +2,6 @@ abstract class StorageRepo {
   // upload profile image on mobile 
   Future<String> uploadProfileImageMobile(String imagePath , String fileName);
   Future<String> uploadProfileImageWeb(String imagePath , String fileName);
+  //
   // uplaod image on web 
 }
