013a1f73742045ba7803c9f20c793a5e898bf6f7
ZiadElsewedy <ziadelsewedy1@gmai.com>
2025-06-04 16:59:52 +0300

    delete welcom snackBar

--- lib/App.dart
+++ lib/App.dart
@@ -112,7 +112,7 @@ class _MyAppState extends State<MyApp> {
               } else if (state is EmailVerificationState) {
                 _showSnackBar(state.message, Colors.blue);
               } else if (state is Authanticated) {
-                _showSnackBar("Welcome back", Colors.green);
+                print("Welcome back , User is logged in");
               }
             },
             builder: (context, state) {
